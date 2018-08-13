dd2ecc4fd6ea20db79794cf3eda6bb8d5631165a
Juan J. Garcia (mini) <juanjo.garcia@gmail.com>
2018-08-13 03:21:02 -0600

    Add Codeanywhere custom git prompt and completion

---
+++ Codeanywhere/.myprofile.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Add colors
+PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ '
+
+# Git completion
+source /usr/share/bash-completion/completions/git
+
+# Git propmt
+GIT_PROMPT_THEME=Single_line_Dark
+GIT_PROMPT_ONLY_IN_REPO=1
+source ~/.bash-git-prompt/gitprompt.sh
---
+++ Codeanywhere/README.md
@@ -0,0 +1,11 @@
+# Codeanywhere
+
+This `.myprofile.sh` it's only for having git completion and git prompt on a codeanywhere container/stack.
+
+## Instructions
+
+Just add it to the end of `.bashrc` file like this:
+```
+source ~/.myprofile.sh
+```
+
