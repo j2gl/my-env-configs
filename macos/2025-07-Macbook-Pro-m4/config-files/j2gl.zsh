051dba7aa5fa6349ef80a65a434a0c9412bfb9bc
j2gl <j2gl.dev@gmail.com>
2026-02-17 15:51:20 +0100

    master: Update custom zsh

--- macos/2025-07-Macbook-Pro-m4/config-files/j2gl.zsh
+++ macos/2025-07-Macbook-Pro-m4/config-files/j2gl.zsh
@@ -1,3 +1,6 @@
+# TO copy to the correct location use;
+# cp macos/2025-07-Macbook-Pro-m4/config-files/j2gl.zsh $ZSH_CUSTOM/j2gl.zsh
+
 # Put files in this folder to add your own custom functionality.
 # See: https://github.com/ohmyzsh/ohmyzsh/wiki/Customization
 #
@@ -15,14 +18,21 @@
 # export PATH="/opt/homebrew/opt/mysql-client/bin:$PATH"
 
 # GPG
-# export GPG_TTY=$(tty)
-# gpgconf --launch gpg-agent
+export GPG_TTY=$(tty)
+gpgconf --launch gpg-agent
 
-export PATH="/opt/homebrew/opt/libpq/bin:$HOME/dev/devtools/bin:$HOME/dev/devtools-aws/bin:$PATH"
+export DEVTOOLS_PROJECT_ROOT=~/dev/devtools
+export PATH="/opt/homebrew/opt/libpq/bin:$HOME/dev/devtools/dist:$HOME/dev/devtools-aws/bin:$PATH"
 
 # THIS MUST BE AT THE END OF THE FILE FOR SDKMAN TO WORK!!!
 export SDKMAN_DIR="$HOME/.sdkman"
 [[ -s "$HOME/.sdkman/bin/sdkman-init.sh" ]] && source "$HOME/.sdkman/bin/sdkman-init.sh"
 
-# TO copy to the correct location use;
-# cp macos/2025-07-Macbook-Pro-m4/config-files/j2gl.zsh $ZSH_CUSTOM/j2gl.zsh
+# yazi
+function y() {
+  local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+  command yazi "$@" --cwd-file="$tmp"
+  IFS= read -r -d '' cwd < "$tmp"
+  [ "$cwd" != "$PWD" ] && [ -d "$cwd" ] && builtin cd -- "$cwd"
+  rm -f -- "$tmp"
+}
